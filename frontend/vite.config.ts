2ebb7fa2c575dbffed1510a1c47ab115d896419d
gridllc <patrick@gridllc.net>
2025-07-10 13:39:00 -0700

    Add: add all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
+
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, '', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     resolve: {
