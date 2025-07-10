9a7c5018929b279ac05fd08d1a40e6cef4b89c15
gridllc <patrick@gridllc.net>
2025-07-10 13:43:43 -0700

    Add: add all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,13 @@
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
-import process from 'node:process'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    define: {
-      // Make the API key available to the client-side code
-      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
-    }
-  }
+  },
 })
